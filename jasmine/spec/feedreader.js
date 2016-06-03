67f4319997527af8f9b5c9c1e716ae3b4ceae3b0
Maddie <maddie.huebner@gmail.com>
2016-06-02 23:20:32 -0500

    Updates to comments and minor fixes

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,8 +26,7 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* TODO: Write a test that loops through each feed
+        /* This fest loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -49,7 +48,7 @@ $(function() {
                     expect(allFeeds.name).toBeDefined();
                     expect(allFeeds.name.length).not.toBe(0);
                 });
-            })
+            });
 
         });
 
@@ -58,15 +57,10 @@ $(function() {
 
 
     
-    /* TODO: Write a new test suite named "The menu" */
-
-    
     describe ('The menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /* This test ensures the menu element is
+         * hidden by default.
          */
         
         //Formatting help from:  https://discussions.udacity.com/t/the-menu-suite-for-hidden-menu-is-not-working/46867/5
@@ -78,10 +72,8 @@ $(function() {
 
 
 
-        /* TODO: Write a test that ensures the menu changes
-         * visibility when the menu icon is clicked. This test
-         * should have two expectations: does the menu display when
-         * clicked and does it hide when clicked again.
+        /* This test ensures the menu changes
+         * visibility when the menu icon is clicked.
          */
 
         //Reference for this test: https://discussions.udacity.com/t/testing-the-dom-in-jasmine/8438
@@ -99,13 +91,9 @@ $(function() {
     });
         
 
-    /* TODO: Write a new test suite named "Initial Entries" */
-
-        /* TODO: Write a test that ensures when the loadFeed
+        /* This test ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
          */
         // Reference:  https://discussions.udacity.com/t/step-13-help-initial-entries/14839/8
 
@@ -122,12 +110,8 @@ $(function() {
             done();
         });
 
-
-    /* TODO: Write a new test suite named "New Feed Selection"
-
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* This test ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
          */
         // Reference:  https://discussions.udacity.com/t/p6-new-feed-selection-test-question-problem/15562/14
 
@@ -145,7 +129,7 @@ $(function() {
              loadFeed(0);
              expect($('.feed').html()).not.toEqual(feedContents);
              done();
-         })
+         });
      });
 
     });
